bb285a55aafad3dedf419bb384b907bcb6d928dd
sylviazhao <sylviazhao09@163.com>
2017-06-05 14:32:31 +0800

    Signed-off-by: sylviazhao <sylviazhao09@163.com>

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/helpTimely');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open',function(callback){
+	console.log('connect successfully')
+})
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
